f9d84ed1386816facebcb439716927a2677f4230
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-13 22:32:35 -0400

    Refactored broadcast

--- connectionHandler.js
+++ connectionHandler.js
@@ -11,22 +11,7 @@ connectionServer.on("listening", () => {
   let join = JSON.stringify(
     action.addJoinMessage({ name: "John Dole", ip: "34.65.75.234" })
   );
-  let joinSignal = new Buffer(join);
-  var joinDatagram = dgram.createSocket("udp4");
-  joinDatagram.bind(() => {
-    joinDatagram.setBroadcast(true);
-  });
-  joinDatagram.send(
-    joinSignal,
-    0,
-    joinSignal.length,
-    UDP_PORT,
-    BROADCAST_IP,
-    (err, bytes) => {
-      if (err) throw err;
-      console.log("JOIN signal successfully sent to all hosts");
-    }
-  );
+  broadcastMessage(join);
 });
 
 // Connection Server receives a JOIN, PING, or LEAVE message
@@ -34,7 +19,8 @@ connectionServer.on("message", (msg, rinfo) => {
   let message = JSON.parse(msg);
   switch (message.type) {
     case action.JOIN:
-    // Broadcast PING message to everyone
+      // Broadcast PING message to everyone
+      console.log("Received JOIN message");
     case action.PING:
     // If user not in online user list, add to list
     case action.LEAVE:
@@ -45,3 +31,25 @@ connectionServer.on("message", (msg, rinfo) => {
 });
 
 connectionServer.bind(UDP_PORT);
+
+// Helper functions
+
+/* Sends msg to all hosts on the network */
+function broadcastMessage(msg) {
+  let message = new Buffer(msg);
+  var datagram = dgram.createSocket("udp4");
+  datagram.bind(() => {
+    datagram.setBroadcast(true);
+  });
+  datagram.send(
+    message,
+    0,
+    message.length,
+    UDP_PORT,
+    BROADCAST_IP,
+    (err, bytes) => {
+      if (err) throw err;
+      console.log(`${JSON.parse(msg).type} successfully sent to all hosts`);
+    }
+  );
+}
