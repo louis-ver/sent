414c5965d229826b10b6cb9d49f8d1266bb06dfe
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-15 19:42:00 -0400

    Implemented PING and JOIN state changes

--- src/server/reducers/sent.js
+++ src/server/reducers/sent.js
@@ -1,34 +1,45 @@
-import {
-  JOIN,
-  PING,
-  LEAVE,
-  ASK,
-  ACCEPT,
-  REFUSE,
-  SEND
-} from "../constants/ActionTypes";
+const actionType = require("../constants/ActionTypes");
 
-const initialState = [
-  {
-    users: [],
-    transfersInProgress: [],
-    completedTransfers: [],
-    requests: []
-  }
-];
+const initialState = {
+  me: { name: "", ip: "" },
+  users: [],
+  transfersInProgress: [],
+  completedTransfers: [],
+  requests: []
+};
 
-export default function sent(state = initialState, action) {
+function sent(state = initialState, action) {
   // Modifies the state according to action type
   // Returns the modified state
   switch (action.type) {
-    case JOIN:
-    case PING:
-    case LEAVE:
-    case ASK:
-    case ACCEPT:
-    case REFUSE:
-    case SEND:
+    // If JOIN, add user to users list
+    case actionType.JOIN:
+      return Object.assign({}, state, {
+        users: [...state.users, { name: action.user.name, ip: action.user.ip }]
+      });
+    // If PING and user not in users list, add to users list
+    case actionType.PING:
+      for (var i = 0; i < state.users.length; i++) {
+        if (
+          state.users[i].name === action.user.name &&
+          state.users[i].ip === action.user.ip
+        ) {
+          return state;
+        }
+      }
+      return Object.assign({}, state, {
+        users: [...state.users, { name: action.user.name, ip: action.user.ip }]
+      });
+    case actionType.LEAVE:
+    case actionType.ASK:
+    case actionType.ACCEPT:
+    case actionType.REFUSE:
+    case actionType.SEND:
     default:
       return state;
   }
 }
+
+module.exports = {
+  sent: sent
+};
