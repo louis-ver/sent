43b4b290ff7a6a8e1c8ab1f4a5d1caa741240a04
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-15 20:41:04 -0400

    Added external constants to fileHandler

--- src/server/servers/fileHandler.js
+++ src/server/servers/fileHandler.js
@@ -1,27 +1,26 @@
 const net = require("net");
 const action = require("../constants/ActionTypes");
+const addresses = require("../constants/Addresses");
 
-TCP_PORT = 8080;
-
-function startTCP(){
-    // Create TCP server
-    const fileServer = net.createServer(socket => {
-      socket.on("data", data => {
-        switch (data.type) {
-          case action.ASK:
-          case action.ACCEPT:
-          case action.REFUSE:
-          case action.SEND:
-          default: // Do default
-        }
-      });
+function startTCP() {
+  // Create TCP server
+  const fileServer = net.createServer(socket => {
+    socket.on("data", data => {
+      switch (data.type) {
+        case action.ASK:
+        case action.ACCEPT:
+        case action.REFUSE:
+        case action.SEND:
+        default: // Do default
+      }
     });
+  });
 
-    fileServer.listen(TCP_PORT, () => {
-      console.log(`File server is listening on port ${TCP_PORT}`);
-    });
+  fileServer.listen(addresses.TCP_PORT, () => {
+    console.log(`File server is listening on port ${addresses.TCP_PORT}`);
+  });
 }
 
 module.exports = {
-    startTCP : startTCP
-}
+  startTCP: startTCP
+};
