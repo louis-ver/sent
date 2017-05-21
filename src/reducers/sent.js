5aa21d38a49657a513ff46cab247b5fc6b0a74ae
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-21 18:41:20 -0400

    Add dummy data to store

--- src/reducers/sent.js
+++ src/reducers/sent.js
@@ -1,8 +1,13 @@
 const actionType = require("../constants/ActionTypes");
+import IncomingRequest from "../classes/incomingRequest.js"
 
 const initialState = {
   me: null,
-  users: [], // {ip : 192.168.0.3, name: Simon, incomingRequests: [incomingRequest]}
+  users: [
+    {ip: "10.0.0.1", name: "Louis-Olivier", incomingRequests: [
+      new IncomingRequest("raiders_of_the_lost_ark.mkv", 1432948),
+      new IncomingRequest("simon_pu.txt", 28),
+      new IncomingRequest("louis_aussi.txt", 2)]}, {ip: "10.0.0.2", name: "Simon", incomingRequests: []}], // {ip : 192.168.0.3, name: Simon, incomingRequests: [incomingRequest]}
   outgoingRequests: [] // {usersTransfers : [userTransfer], file: {file}} //UserTransfer {ip: 192.168.0.3, state: "waiting"}
 };
 
