dcd140b0e6984d0a7a192887e6eb7135a825f692
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-20 21:56:43 -0400

    Convert functional component to class

--- src/components/NewDesign/EnterName.js
+++ src/components/NewDesign/EnterName.js
@@ -1,39 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { addUserFromLogin } from "../../actions/index";
 
 const ip = require("ip");
 
-let EnterName = ({ dispatch, onFormSubmit }) => {
-  let input;
+class EnterName extends Component {
+  render() {
+    let input;
 
-  return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (!input.value.trim()) {
-            return;
-          }
-          // Sets Welcome state as "submitted: true"
-          onFormSubmit(true);
-          // Change Redux state to include "me"
-          dispatch(addUserFromLogin({ name: input.value, ip: ip.address() }));
-        }}
-      >
-        <label>Enter your name:</label>
-        <input
-          ref={node => {
-            input = node;
+    return (
+      <div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            if (!input.value.trim()) {
+              return;
+            }
+            // Sets Welcome state as "submitted: true"
+            this.props.onFormSubmit(true);
+            // Change Redux state to include "me"
+            this.props.dispatch(
+              addUserFromLogin({ name: input.value, ip: ip.address() })
+            );
           }}
-          autoFocus
-        />
-        <button type="submit" className="hidden" />
-      </form>
-    </div>
-  );
-};
+        >
+          <label>Enter your name:</label>
+          <input
+            ref={node => {
+              input = node;
+            }}
+            autoFocus
+          />
+          <button type="submit" className="hidden" />
+        </form>
+      </div>
+    );
+  }
+}
 // Connect to Redux Store
 EnterName = connect()(EnterName);
+
+EnterName.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired
+};
 
 export default EnterName;
