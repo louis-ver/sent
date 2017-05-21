8eb506c4c9713fd783d941e8ef55965a8f6fbaef
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-21 15:14:47 -0400

    Add Request component

--- src/components/NewDesign/Request.js
+++ src/components/NewDesign/Request.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import RequestTypes from "../../constants/RequestTypes"
+import $ from "jquery";
+import "./css/Request.css";
 
 class Request extends Component {
     constructor(props) {
@@ -11,34 +13,48 @@ class Request extends Component {
         }
         this.handleAccept = this.handleAccept.bind(this);
         this.handleRefuse = this.handleRefuse.bind(this);
+        this.transition = this.transition.bind(this);
     }
     handleAccept(event) {
-        this.setState({ 
-            status: RequestTypes.IN_PROGRESS,
+        let c = this;
+        this.transition(() => {
+            // debugger;
+            c.setState({ 
+                status: RequestTypes.IN_PROGRESS,
+            }, () => {$(".statusLabel").fadeIn("fast")});
         });
     }
     handleRefuse(event) {
-        this.setState({
-            status: RequestTypes.REFUSED
+        let c = this;
+        this.transition(() => {
+            c.setState({
+                status: RequestTypes.REFUSED
+            }, () => {$(".statusLabel").fadeIn("fast")});
+        });
+    }
+    transition(intermediaryFunction) {
+        $(".buttons").fadeOut("fast", () => {
+            intermediaryFunction();
         });
     }
     render() {
         let button;
         if (this.state.status === RequestTypes.WAITING) {
             button = (
-                        <div>
-                            <div className="accept" onClick={this.handleAccept}>ACCEPT</div>
-                            <div className="refuse" onClick={this.handleRefuse}>REJECT</div>
+                        <div className="buttons">
+                            <span className="button" onClick={this.handleAccept}>ACCEPT</span>
+                            <span> | </span>
+                            <span className="button" onClick={this.handleRefuse}>REJECT</span>
                         </div>
                     );
         } else if (this.state.status === RequestTypes.COMPLETED) {
-            button = (<div>COMPLETED</div>);
+            button = (<div className="statusLabel">COMPLETED</div>);
         } else if (this.state.status === RequestTypes.FAILED) {
-            button = (<div>FAILED</div>);
+            button = (<div className="statusLabel">FAILED</div>);
         } else if (this.state.status === RequestTypes.REFUSED) {
-            button = (<div>REFUSED</div>);
+            button = (<div className="statusLabel">REFUSED</div>);
         } else {
-            button = (<div className="inProgress">{this.state.progress} / {this.props.fileSize}</div>);
+            button = (<div className="statusLabel">{this.state.progress} / {this.props.fileSize}</div>);
         }
         return(
             <div className="Request">
