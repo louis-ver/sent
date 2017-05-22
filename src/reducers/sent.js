7b3e17d2c494451feb73f00a93ebffbd5a24ef50
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-22 00:57:15 -0400

    Add dummy data to store

--- src/reducers/sent.js
+++ src/reducers/sent.js
@@ -7,7 +7,8 @@ const initialState = {
     {ip: "10.0.0.1", name: "Louis-Olivier", incomingRequests: [
       new IncomingRequest("raiders_of_the_lost_ark.mkv", 1432948),
       new IncomingRequest("simon_pu.txt", 28),
-      new IncomingRequest("louis_aussi.txt", 2)]}, {ip: "10.0.0.2", name: "Simon", incomingRequests: []}], // {ip : 192.168.0.3, name: Simon, incomingRequests: [incomingRequest]}
+      new IncomingRequest("louis_aussi.txt", 2)]}, {ip: "10.0.0.2", name: "Simon", incomingRequests: []},
+      {ip: "10.0.0.3", name: "Henri", incomingRequests: [new IncomingRequest("henri_joue.mov", 87532)]}], // {ip : 192.168.0.3, name: Simon, incomingRequests: [incomingRequest]}
   outgoingRequests: [] // {usersTransfers : [userTransfer], file: {file}} //UserTransfer {ip: 192.168.0.3, state: "waiting"}
 };
 
