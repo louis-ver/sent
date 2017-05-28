955b7e4b0956c43df63f1fe9d025591de5016c00
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-28 00:34:29 -0400

    Reverted back before shitshow

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -8,8 +8,6 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const url = require("url");
 const server = require("./server/server");
-const Store = require("./server/store");
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
