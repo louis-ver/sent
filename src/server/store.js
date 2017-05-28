becd9124bcb81dcacaf768243f66e4d83686630c
Louis-Olivier Guérin <hi@louisolivier.com>
2017-05-27 22:35:05 -0400

    Testing Global

--- src/server/store.js
+++ src/server/store.js
@@ -1,8 +1,20 @@
 const { createStore } = require('redux');
 const { sent } = require("../reducers/sent");
 
-var store = createStore(sent);
+// var store = createStore(sent);
+//
+// module.exports = {
+//   Store: store
+// }
+class Store {
+  constructor() {
+    this._store = createStore(sent);
+  }
+}
+
+const instance = new Store();
+Object.freeze(instance);
 
 module.exports = {
-  Store: store
+  Store: instance._store
 }
