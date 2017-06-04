c18dc4f021ef2de557facc578fd120e4f0c5a2ba
simon-dube <simon.dube@hotmail.com>
2017-06-04 18:41:42 -0400

    Close working?

--- src/server/servers/connectionServer.js
+++ src/server/servers/connectionServer.js
@@ -4,4 +4,8 @@ const addresses = require("../../constants/Addresses");
 var connectionServer = dgram.createSocket("udp4");
 connectionServer.bind(addresses.UDP_PORT);
 
+window.onbeforeunload = () => {
+    connectionServer.close();
+};
+
 module.exports = {connectionServer};
