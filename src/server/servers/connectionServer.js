275de90cecb591d2829f0d80447c3e874820da63
simon-dube <simon.dube@hotmail.com>
2017-06-04 19:37:16 -0400

    Close work

--- src/server/servers/connectionServer.js
+++ src/server/servers/connectionServer.js
@@ -1,11 +1,13 @@
 const dgram = require("dgram");
 const addresses = require("../../constants/Addresses");
+const $ = require("jquery");
 
 var connectionServer = dgram.createSocket("udp4");
 connectionServer.bind(addresses.UDP_PORT);
 
-window.onbeforeunload = () => {
-    connectionServer.close();
-};
+$(window).on("beforeunload", e => {
+    connectionServer.close(() => window.close());
+    return null;
+});
 
 module.exports = {connectionServer};
