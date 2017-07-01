0a0468678fd61a0c287be8ccaf6b2760e89297dd
simon-dube <simon.dube@hotmail.com>
2017-07-01 11:14:27 -0400

    TCP Attemps take 3

--- src/server/services/fileService.js
+++ src/server/services/fileService.js
@@ -6,10 +6,10 @@ const { Propose } = require("../actions/propose");
 
 function proposeTransfer(outgoingRequest){
     let propose = new Propose(outgoingRequest.file);
-    outgoingRequest.users.forEach(user =>{
+    outgoingRequest.users.forEach(ur =>{
         debugger;
         let client = new net.Socket();
-        client.connect(Addresses.TCP_PORT, user.ip, () => {
+        client.connect(Addresses.TCP_PORT, ur.user.ip, () => {
             client.write(propose);
             client.destroy();
         })
