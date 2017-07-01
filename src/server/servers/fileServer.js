03b05b6a2c6c97acc22a82e372cc4318fc23902e
simon-dube <simon.dube@hotmail.com>
2017-07-01 12:14:31 -0400

    Working2?

--- src/server/servers/fileServer.js
+++ src/server/servers/fileServer.js
@@ -1,6 +1,8 @@
 const net = require('net');
 const addresses = require("../../constants/Addresses");
+const ip = require("ip");
 
-const fileServer = net.createConnection({port: addresses.TCP_PORT, localPort: 8080});
+
+const fileServer = net.createConnection({port: addresses.TCP_PORT, localPort: 8080, host: ip.address(), localAddress: ip.address()});
 
 module.exports = {fileServer};
