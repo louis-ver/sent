2aa0b23eb5458538d29386fc9daf47c8e92e35a0
Louis-Olivier Guérin <hi@louisolivier.com>
2017-07-02 19:56:02 -0400

    Fix File

--- src/classes/DTO/incomingRequestDTO.js
+++ src/classes/DTO/incomingRequestDTO.js
@@ -1,7 +1,9 @@
+import InfoFile from "../../server/actions/classes/infoFile";
+
 class IncomingRequestDTO {
     constructor(outgoingRequestId, file){
         this.id = outgoingRequestId;
-        this.file = file;
+        this.file = new InfoFile(file);
     }
 }
 
